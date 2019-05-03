02336e0fcc3d9510c802c5991a88ae461ea67186
Wilfredo Sánchez <wsanchez@wsanchez.net>
2019-05-03 16:26:17 -0700

    Make buildOptions() and smtpOptions() callable so we can change whether they are required.

--- src/deploy/notify/smtp.py
+++ src/deploy/notify/smtp.py
@@ -152,97 +152,99 @@ def validateRepositoryID(
     return (value, organization, project)
 
 
-buildOptions = composedOptions(
-    commandOption(
-        "--project-name",
-        envvar="PROJECT_NAME",
-        help="project name",
-        type=str, metavar="<name>",
-        prompt=False, required=False,
-    ),
-    commandOption(
-        "--repository-id",
-        envvar="REPOSITORY_ID",
-        help="repository",
-        type=str, metavar="<organization>/<project>",
-        prompt=True, required=True,
-        callback=validateRepositoryID,
-    ),
-    commandOption(
-        "--build-number",
-        envvar="BUILD_NUMBER",
-        help="build number",
-        type=str, metavar="<number>",
-        prompt=True, required=True,
-    ),
-    commandOption(
-        "--build-url",
-        envvar="BUILD_URL",
-        help="build URL",
-        type=str, metavar="<url>",
-        prompt=True, required=True,
-    ),
-    commandOption(
-        "--commit-id",
-        envvar="COMMIT_ID",
-        help="commit ID",
-        type=str, metavar="<id>",
-        prompt=True, required=True,
-    ),
-    commandOption(
-        "--commit-message",
-        envvar="COMMIT_MESSAGE",
-        help="commit message",
-        type=str, metavar="<message>",
-        prompt=True, required=True,
-    ),
-)
+def buildOptions(required: bool = True) -> Callable[..., Callable]:
+    return composedOptions(
+        commandOption(
+            "--project-name",
+            envvar="PROJECT_NAME",
+            help="project name",
+            type=str, metavar="<name>",
+            prompt=False, required=False,
+        ),
+        commandOption(
+            "--repository-id",
+            envvar="REPOSITORY_ID",
+            help="repository",
+            type=str, metavar="<organization>/<project>",
+            prompt=required, required=required,
+            callback=validateRepositoryID,
+        ),
+        commandOption(
+            "--build-number",
+            envvar="BUILD_NUMBER",
+            help="build number",
+            type=str, metavar="<number>",
+            prompt=required, required=required,
+        ),
+        commandOption(
+            "--build-url",
+            envvar="BUILD_URL",
+            help="build URL",
+            type=str, metavar="<url>",
+            prompt=required, required=required,
+        ),
+        commandOption(
+            "--commit-id",
+            envvar="COMMIT_ID",
+            help="commit ID",
+            type=str, metavar="<id>",
+            prompt=required, required=required,
+        ),
+        commandOption(
+            "--commit-message",
+            envvar="COMMIT_MESSAGE",
+            help="commit message",
+            type=str, metavar="<message>",
+            prompt=required, required=required,
+        ),
+    )
 
 
-smtpOptions = composedOptions(
-    commandOption(
-        "--smtp-host",
-        envvar="NOTIFY_SMTP_HOST",
-        help="SMTP server host name",
-        type=str, metavar="<host>",
-        prompt=True, required=True,
-    ),
-    commandOption(
-        "--smtp-port",
-        envvar="NOTIFY_SMTP_PORT",
-        help="SMTP server port",
-        type=int, metavar="<port>",
-        prompt=False, required=False, default=465,
-    ),
-    commandOption(
-        "--smtp-user",
-        envvar="NOTIFY_SMTP_USER",
-        help="SMTP user name",
-        type=str, metavar="<user>",
-        prompt=True, required=True,
-    ),
-    commandOption(
-        "--smtp-password",
-        envvar="NOTIFY_SMTP_PASSWORD",
-        help="SMTP user password",
-        type=str, metavar="<password>",
-        prompt=True, required=True,
-    ),
-    commandOption(
-        "--sender",
-        envvar="NOTIFY_EMAIL_SENDER",
-        help="email sender address",
-        type=str, metavar="<address>",
-        prompt=True, required=True,
-    ),
-    commandOption(
-        "--recipient",
-        envvar="NOTIFY_EMAIL_RECIPIENT",
-        help="email recipient address",
-        type=str, metavar="<address>",
-        prompt=True, required=True,
-    ),
-)
+def smtpOptions(required: bool = True) -> Callable[..., Callable]:
+    return composedOptions(
+        commandOption(
+            "--smtp-host",
+            envvar="NOTIFY_SMTP_HOST",
+            help="SMTP server host name",
+            type=str, metavar="<host>",
+            prompt=required, required=required,
+        ),
+        commandOption(
+            "--smtp-port",
+            envvar="NOTIFY_SMTP_PORT",
+            help="SMTP server port",
+            type=int, metavar="<port>",
+            prompt=False, required=False, default=465,
+        ),
+        commandOption(
+            "--smtp-user",
+            envvar="NOTIFY_SMTP_USER",
+            help="SMTP user name",
+            type=str, metavar="<user>",
+            prompt=required, required=required,
+        ),
+        commandOption(
+            "--smtp-password",
+            envvar="NOTIFY_SMTP_PASSWORD",
+            help="SMTP user password",
+            type=str, metavar="<password>",
+            prompt=required, required=required,
+        ),
+        commandOption(
+            "--sender",
+            envvar="NOTIFY_EMAIL_SENDER",
+            help="email sender address",
+            type=str, metavar="<address>",
+            prompt=required, required=required,
+        ),
+        commandOption(
+            "--recipient",
+            envvar="NOTIFY_EMAIL_RECIPIENT",
+            help="email recipient address",
+            type=str, metavar="<address>",
+            prompt=required, required=required,
+        ),
+    )
 
 
 @commandGroup()
@@ -266,8 +268,8 @@ def main(ctx: ClickContext, profile: Optional[str]) -> None:
 
 
 @main.command()
-@buildOptions
-@smtpOptions
+@buildOptions()
+@smtpOptions()
 def staging(
     project_name: Optional[str], repository_id: Tuple[str, str, str],
     build_number: str, build_url: str, commit_id: str, commit_message: str,
