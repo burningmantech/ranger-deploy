914d930b08b01eaa7d60395750ae896732e2e948
requires.io <support@requires.io>
2020-01-21 12:45:02 -0800

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -143,7 +143,7 @@ install_requirements = [
     "s3transfer==0.3.1",
     "six==1.14.0",
     "smmap2==2.0.5",
-    "urllib3==1.25.7",
+    "urllib3==1.25.8",
     "websocket-client==0.57.0",
     "zope.interface==4.7.1",
 ]
