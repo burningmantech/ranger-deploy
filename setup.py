68f126ff4e01e60ded8bb28aab197e56ec0fb0ac
requires.io <support@requires.io>
2020-02-16 19:18:39 -0800

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -132,7 +132,7 @@ install_requirements = [
     "chardet==3.0.4",
     "constantly==15.1.0",
     "docutils==0.16",
-    "gitdb2==3.0.1",
+    "gitdb2==3.0.2",
     "hyperlink==19.0.0",
     "idna==2.8",
     "incremental==17.5.0",
