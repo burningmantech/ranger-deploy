0b89444c52b1b1719347be997b90f577d253c7f8
requires.io <support@requires.io>
2020-02-24 16:41:21 -0800

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -136,7 +136,7 @@ install_requirements = [
     "hyperlink==19.0.0",
     "idna==2.9",
     "incremental==17.5.0",
-    "jmespath==0.9.4",
+    "jmespath==0.9.5",
     "PyHamcrest==2.0.0",
     "python-dateutil==2.8.1",
     "requests==2.23.0",
