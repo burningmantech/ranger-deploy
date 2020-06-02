6368ed28d38bce0c83fe356c1beb24f4e4c98885
requires.io <support@requires.io>
2020-06-02 07:18:27 -0700

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ install_requirements = [
     "attrs==19.3.0",
     "boto3>=1.13,<1.14",
     "Click==7.1.2",
-    "docker==4.2.0",  # [tls]
+    "docker==4.2.1",  # [tls]
     "GitPython==3.1.3",
     "Twisted==20.3.0",
 
