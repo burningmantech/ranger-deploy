e76566eab6e03c192097427c6d5f17c4fe07afc7
requires.io <support@requires.io>
2021-04-22 14:12:19 -0700

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -140,7 +140,7 @@ install_requirements = [
     "PyHamcrest==2.0.2",
     "python-dateutil==2.8.1",
     "requests==2.25.1",
-    "s3transfer==0.4.1",
+    "s3transfer==0.4.2",
     "six==1.15.0",
     "smmap2==3.0.1",
     "urllib3==1.26.4",
